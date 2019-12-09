27f1039b259ab7a32f2559ae7c9ec6cd9f580fcf
Abdelwahab Kawafi <ak18001@it073144.users.bris.ac.uk>
2019-12-09 10:42:08 +0000

    adding first file

---
+++ clean.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+from tqdm import tqdm
+
+def remove_keymap_conflicts(new_keys_set):
+    for prop in plt.rcParams:
+        if prop.startswith('keymap.'):
+            keys = plt.rcParams[prop]
+            remove_list = set(keys) & new_keys_set
+            for key in remove_list:
+                keys.remove(key)
+
+def multi_slice_viewer(volume):
+    fig, ax = plt.subplots()
+    ax.volume = volume
+    ax.index = volume.shape[0] // 2
+    ax.imshow(volume[ax.index])
+    fig.canvas.mpl_connect('key_press_event', process_key)
+
+def process_key(event):
+    fig = event.canvas.figure
+    ax = fig.axes[0]
+    if event.key == 'j':
+        previous_slice(ax)
+    elif event.key == 'k':
+        next_slice(ax)
+    fig.canvas.draw()
+
+def previous_slice(ax):
+    volume = ax.volume
+    ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
+    ax.images[0].set_array(volume[ax.index])
+
+def next_slice(ax):
+    volume = ax.volume
+    ax.index = (ax.index + 1) % volume.shape[0]
+    ax.images[0].set_array(volume[ax.index])
+
+def readCT(name, direc):
+	pass
+
+ct = []
+print('Loading...')
+for i in tqdm(range(500)):
+	ct.append(cv2.imread('/home/ak18001/Data/uCT/EK_208_215/EK_208_215_'+(str(i).zfill(4))+'.tif'))
+
+ct = np.array(ct)
+
+multi_slice_viewer(ct)
+
+
+
+
