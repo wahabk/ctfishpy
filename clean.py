f8e7442c988b35173d60a97cfb1b47c7c02a8186
Abdelwahab Kawafi <ak18001@it073144.users.bris.ac.uk>
2019-12-09 13:43:09 +0000

    added utility with slice viewer

--- clean.py
+++ clean.py
@@ -1,54 +1,27 @@
-
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 from tqdm import tqdm
-
-def remove_keymap_conflicts(new_keys_set):
-    for prop in plt.rcParams:
-        if prop.startswith('keymap.'):
-            keys = plt.rcParams[prop]
-            remove_list = set(keys) & new_keys_set
-            for key in remove_list:
-                keys.remove(key)
-
-def multi_slice_viewer(volume):
-    fig, ax = plt.subplots()
-    ax.volume = volume
-    ax.index = volume.shape[0] // 2
-    ax.imshow(volume[ax.index])
-    fig.canvas.mpl_connect('key_press_event', process_key)
-
-def process_key(event):
-    fig = event.canvas.figure
-    ax = fig.axes[0]
-    if event.key == 'j':
-        previous_slice(ax)
-    elif event.key == 'k':
-        next_slice(ax)
-    fig.canvas.draw()
-
-def previous_slice(ax):
-    volume = ax.volume
-    ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
-    ax.images[0].set_array(volume[ax.index])
-
-def next_slice(ax):
-    volume = ax.volume
-    ax.index = (ax.index + 1) % volume.shape[0]
-    ax.images[0].set_array(volume[ax.index])
+from utility import IndexTracker
 
 def readCT(name, direc):
 	pass
 
 ct = []
-print('Loading...')
-for i in tqdm(range(500)):
-	ct.append(cv2.imread('/home/ak18001/Data/uCT/EK_208_215/EK_208_215_'+(str(i).zfill(4))+'.tif'))
-
+slices_to_read = 250
+for i in tqdm(range(1500,1999)):
+	ct.append(cv2.imread('../../Data/uCT/EK_208_215/EK_208_215_'+(str(i).zfill(4))+'.tif', cv2.IMREAD_GRAYSCALE))
 ct = np.array(ct)
+#ct = np.moveaxis(ct, 0, -1)
 
-multi_slice_viewer(ct)
+fig, ax = plt.subplots(1, 1)
+tracker = IndexTracker(ax, ct.T)
+fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+plt.show()
+
+
+
+
 
 
 
---
+++ utility.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class IndexTracker(object):
+    def __init__(self, ax, X):
+        self.ax = ax
+        ax.set_title('use scroll wheel to navigate images')
+
+        self.X = X
+        rows, cols, self.slices = X.shape
+        self.ind = self.slices//2
+
+        self.im = ax.imshow(self.X[:, :, self.ind])
+        self.update()
+
+    def onscroll(self, event):
+        if event.button == 'up':
+            self.ind = (self.ind + 10) % self.slices
+        else:
+            self.ind = (self.ind - 10) % self.slices
+        self.update()
+
+    def update(self):
+        self.im.set_data(self.X[:, :, self.ind])
+        self.ax.set_ylabel('slice %s' % self.ind)
+        self.im.axes.figure.canvas.draw()
