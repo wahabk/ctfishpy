4cd920ae448d6b9b7f15c825ce9366d3c992bc9c
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2021-02-15 17:55:51 +0000

    tversky day 2

--- ctfishpy/model/Unet.py
+++ ctfishpy/model/Unet.py
@@ -3,7 +3,7 @@ from ..controller import CTreader, cc
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-import json, codecs, pickle
+import json, codecs, pickle, csv
 import segmentation_models as sm
 import tensorflow.keras as keras
 import tensorflow.keras.backend as K
@@ -17,43 +17,54 @@ sm.set_framework('tf.keras')
 class Unet():
 	def __init__(self, organ):
 		self.shape = (224,224)
-		self.roiZ = 110
+		self.roiZ = 100
 		self.organ = organ
-		self.val_steps = 5
 		self.batch_size = 32
-		
-		self.epochs = 25
-		self.lr = 1e-4
+		self.epochs = 60
+		self.lr = 1e-5
+		self.pretrain = True #write this into logic
 		self.BACKBONE = 'resnet34'
-		self.weights = 'imagenet'
 		self.weightspath = 'output/Model/unet_checkpoints.hdf5'
-		# 'output/Model/unet_checkpoints.hdf5'
-		self.encoder_freeze=True
+		self.encoder_freeze=False
 		self.nclasses = 3
 		self.activation = 'softmax'
 		self.class_weights = np.array([0.5,2,2.5])
-		self.metrics = [sm.metrics.FScore(threshold=0.5, class_indexes=None)]
+		self.metrics = [sm.metrics.FScore(threshold=0.5), sm.metrics.IOUScore(threshold=0.5)]
 		self.rerun = False
-		self.slice_weighting = 8
+		self.slice_weighting = 1
+		self.alpha = 0.3
+		self.loss=self.focal_tversky_loss
+		# 'output/Model/unet_checkpoints.hdf5'
+		members = {attr: getattr(self, attr) for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")}
+		print(members)
+		
 
+	def saveParams(self):
+		members = {attr: getattr(self, attr) for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")}
+		with open('output/Model/trainingParameters.csv', 'w') as f:  
+			w = csv.DictWriter(f, members.keys())
+			w.writeheader()
+			w.writerow(members)
 
 	def getModel(self):
 		# dice_loss = sm.losses.DiceLoss(class_weights=self.class_weights) 
 		# focal_loss = sm.losses.CategoricalFocalLoss()	
 		# total_loss = dice_loss + (1 * focal_loss)
-
+		if self.pretrain:
+			self.weights = 'imagenet'
+		else:
+			self.weights = None
 		optimizer = Adam(learning_rate=self.lr)
 
-		#base_model = sm.Unet(self.BACKBONE, encoder_weights=self.weights, classes=self.nclasses, activation=self.activation, encoder_freeze=self.encoder_freeze)
-		base_model = sm.Unet(self.BACKBONE, encoder_weights=None, classes=self.nclasses, activation=self.activation, encoder_freeze=True)
+		base_model = sm.Unet(self.BACKBONE, encoder_weights=self.weights, classes=self.nclasses, activation=self.activation, encoder_freeze=self.encoder_freeze)
+		#base_model = sm.Unet(self.BACKBONE, encoder_weights=None, classes=self.nclasses, activation=self.activation, encoder_freeze=False)
 		inp = Input(shape=(self.shape[0], self.shape[1], 1))
 		l1 = Conv2D(3, (1, 1))(inp) # map N channels data to 3 channels
 		out = base_model(l1)
-
-		
 		model = Model(inp, out, name=base_model.name)
+
 		if self.rerun: model.load_weights(self.weightspath)
-		model.compile(optimizer=optimizer, loss=focal_tversky_loss, metrics=self.metrics)
+		model.compile(optimizer=optimizer, loss=self.focal_tversky_loss, metrics=self.metrics)
 		return model
 
 	def train(self, sample, val_sample):
@@ -62,13 +73,13 @@ class Unet():
 		
 		
 		trainStartTime = time.strftime("%Y-%m-%d-%H-%M") #save time that you started training
-		data_gen_args = dict(rotation_range=1, # degrees
-					width_shift_range=1, #pixels
-					height_shift_range=1,
-					shear_range=1, #degrees
-					zoom_range=0.1, # up to 1
+		data_gen_args = dict(rotation_range=2, # degrees
+					width_shift_range=2, #pixels
+					height_shift_range=2,
+					shear_range=2, #degrees
+					zoom_range=0.2, # up to 1
 					horizontal_flip=True,
-					vertical_flip = True,
+					vertical_flip = False,
 					fill_mode='constant',
 					cval = 0)
 
@@ -78,11 +89,12 @@ class Unet():
 						fish_nums = self.sample)
 
 		xval, yval, val_sample_weights = self.dataGenie(batch_size = self.batch_size,
-				data_gen_args = dict(),
-				fish_nums = self.val_sample)
+						data_gen_args = dict(),
+						fish_nums = self.val_sample)
+		
 		model = self.getModel()
-		self.steps_per_epoch = int((self.roiZ * len(self.sample)) / self.batch_size)
-		print('\n\n\nSTEPS\n\n\n', self.steps_per_epoch, self.roiZ, len(xtrain))
+		self.steps_per_epoch = int(len(xtrain) / self.batch_size)
+		self.val_steps = int(len(xval) / self.batch_size)
 
 		model_checkpoint = ModelCheckpoint(self.weightspath, monitor = 'loss', verbose = 1, save_best_only = True)
 
@@ -92,11 +104,17 @@ class Unet():
 			model_checkpoint
 		]
 		
-		history = model.fit(xtrain, y=ytrain, validation_data=(xval, yval), steps_per_epoch = self.steps_per_epoch, 
+		history = model.fit(xtrain, y=ytrain, sample_weight=sample_weights, validation_data=(xval, yval, val_sample_weights), steps_per_epoch = self.steps_per_epoch, 
 						epochs = self.epochs, callbacks=callbacks, validation_steps=self.val_steps)
+		
+		# test_sample=
+		# self.score = model.evaluate()
+		self.saveParams()
 		self.history = history.history
 		self.history['time'] = trainStartTime
 		self.saveHistory(f'output/Model/History/{trainStartTime}_history.json', self.history)
+
+
 
 	def makeLossCurve(self, history=None):
 		if history == None: history = self.history
@@ -112,37 +130,19 @@ class Unet():
 		plt.legend(metrics, loc='upper left')
 		plt.savefig('output/Model/loss_curves/'+history['time']+'_loss.png')
 
-	def saveHistory(self, path, history):
-		'''
-		from https://stackoverflow.com/questions/41061457/keras-how-to-save-the-training-history-attribute-of-the-history-object
-		'''
-		with open(path, 'wb') as file_pi:
-			pickle.dump(history, file_pi)
-
-	def loadHistory(self, path):
-		'''
-		load history saved as pickle file
-
-		parameters:
-		path
-
-		from https://stackoverflow.com/questions/41061457/keras-how-to-save-the-training-history-attribute-of-the-history-object
-		'''
-		with open(path, 'rb') as file_pi:
-			history = pickle.load(file_pi)
-		return history
-
-	def test(self):
-		# TODO setup proper end test
-		pass
-
 	def predict(self, n):
-		base_model = sm.Unet(self.BACKBONE, classes=self.nclasses, activation=self.activation, encoder_freeze=self.encoder_freeze)
+		base_model = sm.Unet(self.BACKBONE, classes=self.nclasses, activation=self.activation)
 		inp = Input(shape=(self.shape[0], self.shape[1], 1))
 		l1 = Conv2D(3, (1, 1))(inp) # map N channels data to 3 channels
 		out = base_model(l1)
 		model = Model(inp, out, name=base_model.name)
 		model.load_weights(self.weightspath)
+		
+		# base_model = sm.Unet(self.BACKBONE, encoder_weights=None, classes=self.nclasses, activation=self.activation, encoder_freeze=True)
+		# inp = Input(shape=(self.shape[0], self.shape[1], 1))
+		# l1 = Conv2D(3, (1, 1))(inp) # map N channels data to 3 channels
+		# out = base_model(l1)
+
 
 		test = self.testGenie(n)
 		results = model.predict(test, self.batch_size) # read about this one
@@ -171,7 +171,6 @@ class Unet():
 		roiZ=self.roiZ
 		roiSize=self.shape[0]
 		seed = 2
-		num_classes = self.nclasses
 
 		ct_list, label_list = [], []
 		for num in fish_nums:
@@ -197,8 +196,8 @@ class Unet():
 				label[label == 2] = 0
 				label[label == 3] = 2
 
-			new_mask = np.zeros(label.shape + (num_classes,))
-			for i in range(num_classes):
+			new_mask = np.zeros(label.shape + (self.nclasses,))
+			for i in range(self.nclasses):
 				#for one pixel in the image, find the class in mask and convert it into one-hot vector
 				new_mask[label == i,i] = 1
 			
@@ -249,11 +248,13 @@ class Unet():
 		total = ydata.shape[0]
 
 		temp = np.zeros(ydata.shape[:-1], dtype = 'uint8')
-		for i in range(self.nclasses):
+		for i in [1,2]:
 			y = ydata[:, :, :, i]
 			temp[y==1] = i
 
-		distrib = [np.any(temp[i]) for i in range(total)]
+		distrib1 = [np.any(temp[i]) for i in range(total)]
+		distrib2 = [np.any(temp[i]) for i in range(total)]
+		distrib = [any(tup) for tup in zip(distrib1, distrib2)]
 		distrib = np.array(distrib)
 		sample_weights = np.ones(shape=(total))
 		sample_weights[distrib == True] = self.slice_weighting
@@ -268,11 +269,11 @@ class Unet():
 
 	def testGenie(self, n):
 		ctreader = CTreader()
-		# center, error = cc(num, template, thresh=200, roiSize=50)
-		centres_path = ctreader.dataset_path / f'Metadata/cc_centres_{self.organ}.json'
+		template = ctreader.read_label(self.organ, 0)
+		# center = cc(n, template, thresh=80, roiSize=224)
+		centres_path = ctreader.centres_path
 		with open(centres_path, 'r') as fp:
-			centres = json.load(fp)
-		
+			centres = json.load(fp)	
 		center = centres[str(n)]
 		z_center = center[0] # Find center of cc result and only read roi from slices
 		roiZ=self.roiZ
@@ -282,14 +283,66 @@ class Unet():
 		ct = ctreader.crop_around_center3d(ct, center = center, roiSize=roiSize, roiZ=roiZ)
 		ct = np.array([_slice / 65535 for _slice in ct], dtype='float32') # Normalise 16 bit slices
 		ct = ct[:,:,:,np.newaxis] # add final axis to show datagens its grayscale
+		print(ct.shape)
 		return ct
 
+	def saveHistory(self, path, history):
+		'''
+		from https://stackoverflow.com/questions/41061457/keras-how-to-save-the-training-history-attribute-of-the-history-object
+		'''
+		with open(path, 'wb') as file_pi:
+			pickle.dump(history, file_pi)
 
+	def loadHistory(self, path):
+		'''
+		load history saved as pickle file
+
+		parameters:
+		path
+
+		from https://stackoverflow.com/questions/41061457/keras-how-to-save-the-training-history-attribute-of-the-history-object
+		'''
+		with open(path, 'rb') as file_pi:
+			history = pickle.load(file_pi)
+		return history
+
+	def test(self):
+		# TODO setup proper end test
+		pass
+
+	def tversky(self, y_true, y_pred, smooth=1, alpha=None):
+		'''
+		Ref: salehi17, "Twersky loss function for image segmentation using 3D FCDN"
+		-> the score is computed for each class separately and then summed
+		alpha=beta=0.5 : dice coefficient
+		alpha=beta=1   : tanimoto coefficient (also known as jaccard)
+		alpha+beta=1   : produces set of F*-scores
+		implemented by E. Moebel, 06/04/18
+
+		From https://github.com/keras-team/keras/issues/9395
+		'''
+		alpha=self.alpha
+		beta = 1-alpha
+		y_true_pos = K.flatten(y_true)
+		y_pred_pos = K.flatten(y_pred)
+		true_pos = K.sum(y_true_pos * y_pred_pos)
+		false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+		false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
+		return (true_pos + smooth) / (true_pos + alpha*false_pos+ beta*false_neg + smooth)
+
+
+	def tversky_loss(self, y_true, y_pred):
+		return 1 - self.tversky(y_true, y_pred)
+
+
+	def focal_tversky_loss(self, y_true, y_pred, gamma=0.75):
+		tv = self.tversky(y_true, y_pred)
+		return K.pow((1 - tv), gamma)
 
 def lr_scheduler(epoch, learning_rate):
-	decay_rate = 1
-	decay_step = 13
-	if epoch % decay_step == 0 and epoch != 0 and epoch != 1:
+	decay_rate =  1
+	decay_step = 12
+	if epoch == decay_step and epoch != 0 and epoch != 1:
 		return learning_rate * decay_rate
 	return learning_rate
 
@@ -299,29 +352,3 @@ def fixFormat(batch, label = False):
 	if label: return np.squeeze(batch.astype('uint8'), axis = 3)
 
 
-def tversky(y_true, y_pred, smooth=1, alpha=0.8):
-	'''
-	Ref: salehi17, "Twersky loss function for image segmentation using 3D FCDN"
-	-> the score is computed for each class separately and then summed
-	alpha=beta=0.5 : dice coefficient
-	alpha=beta=1   : tanimoto coefficient (also known as jaccard)
-	alpha+beta=1   : produces set of F*-scores
-	implemented by E. Moebel, 06/04/18
-
-	From https://github.com/keras-team/keras/issues/9395
-	'''
-	y_true_pos = K.flatten(y_true)
-	y_pred_pos = K.flatten(y_pred)
-	true_pos = K.sum(y_true_pos * y_pred_pos)
-	false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
-	false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
-	return (true_pos + smooth) / (true_pos + alpha * false_neg + (1 - alpha) * false_pos + smooth)
-
-
-def tversky_loss(y_true, y_pred):
-	return 1 - tversky(y_true, y_pred)
-
-
-def focal_tversky_loss(y_true, y_pred, gamma=0.75):
-	tv = tversky(y_true, y_pred)
-	return K.pow((1 - tv), gamma)
--- train.py
+++ train.py
@@ -1,15 +1,17 @@
 import ctfishpy
 
 
+# cc doesnt work on 464 385 421 423 
+wahab_samples 	= [337,341,462,464,385,200,218,330]
+mariel_samples	= [242,259,459,463,257, 256] # removed
+zac_samples		= [443,40,78,423] # removing 527, 530, 582, 589, 421, 240, 461, 277
+sample = wahab_samples+mariel_samples
+val_samples = zac_samples
 
-wahab_samples 	= [337,341,462,40,78, 464, 385] # cc doesnt work on 464 385
-mariel_samples	= [242,256,259,459,463,530,589,421,423] # 421 423 removed
-zac_samples		= [257,443,461,527,582]
-sample = wahab_samples+mariel_samples+zac_samples
-val_samples= [200,218,240,277,330] # from me
-
-unet = ctfishpy.model.Unet('Otoliths')
+unet = ctfishpy.Unet('Otoliths')
 unet.rerun = True
+unet.epochs= 20
+# unet.lr = 1e-6
 unet.train(sample, val_samples)
 
 unet.makeLossCurve()
