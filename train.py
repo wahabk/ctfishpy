5e76451c75d5c9a8723f1510d43ed519d7890fb4
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2021-03-04 10:15:20 +0000

    final MP

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import ctfishpy
 import segmentation_models as sm
 import numpy as np
+
+
 # cc doesnt work on 464 385 421 423, 78
 #256 mariel needs to be redone
 wahab_samples 	= [78,200,240,277,330,337,341,462]
@@ -10,6 +12,12 @@ zac_samples		= [257,443,218]
 sample = wahab_samples+mariel_samples
 val_sample = [464,364,385,40,461]
 
+unet = ctfishpy.Unet('Otoliths')
+unet.weightsname = 'unet'
+unet.comment = 'no_class_weights'
+unet.loss = sm.losses.DiceLoss()
+unet.train(sample, val_sample, zac_samples)
+unet.makeLossCurve()
 
 # params = [[1e-5, 150, 'cross_entropy', 	0.7, 	[1,1,1],			32, True],
 # 			[1e-5, 150, 'dice_loss', 		0.7, 	[1,1,1],			32, True],
@@ -23,35 +31,35 @@ val_sample = [464,364,385,40,461]
 # 			[1e-5, 150, 'tversky',			0.5,	[1,1,1],			32, True]]
 
 
-params = [	[1e-5, 150, 'tversky', 0.5, [1,1,1], 16 ,True],
-			[1e-5, 150, 'tversky', 0.5, [1,1,1], 10 ,True],
-			[1e-5, 150, 'tversky', 0.5, [1,1,1], 25 ,True],
-			[1e-3, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
-			[1e-4, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
-			[1e-6, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
-			[1e-5, 150, 'tversky', 0.5, [1,1,1], 16 ,False]]
+# params = [	[1e-5, 150, 'tversky', 0.5, [1,1,1], 16 ,True],
+# 			[1e-5, 150, 'tversky', 0.5, [1,1,1], 10 ,True],
+# 			[1e-5, 150, 'tversky', 0.5, [1,1,1], 25 ,True],
+# 			[1e-3, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
+# 			[1e-4, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
+# 			[1e-6, 150, 'tversky', 0.5, [1,1,1], 32 ,True],
+# 			[1e-5, 150, 'tversky', 0.5, [1,1,1], 16 ,False]]
 
 
 
 
-for lr, epochs, loss, alpha, class_weights, batch_size, encoder_freeze in params:
-	unet = ctfishpy.Unet('Otoliths')
-	unet.weightsname = 'unet'
-	unet.comment = 'Mariel_parameters'
-	unet.lr = lr
-	unet.epochs = epochs
-	unet.alpha = alpha
-	unet.class_weights = np.array(class_weights)
-	unet.batch_size = batch_size
-	unet.encoder_freeze = encoder_freeze
+# for lr, epochs, loss, alpha, class_weights, batch_size, encoder_freeze in params:
+# 	unet = ctfishpy.Unet('Otoliths')
+# 	unet.weightsname = 'unet'
+# 	unet.comment = 'Mariel_parameters'
+# 	unet.lr = lr
+# 	unet.epochs = epochs
+# 	unet.alpha = alpha
+# 	unet.class_weights = np.array(class_weights)
+# 	unet.batch_size = batch_size
+# 	unet.encoder_freeze = encoder_freeze
 
-	if loss == 'tversky': unet.loss = unet.multi_class_tversky_loss
-	if loss == 'dice_loss': unet.loss = sm.losses.DiceLoss(class_weights=unet.class_weights)
-	if loss == 'cross_entropy': unet.loss = sm.losses.categorical_crossentropy
+# 	if loss == 'tversky': unet.loss = unet.multi_class_tversky_loss
+# 	if loss == 'dice_loss': unet.loss = sm.losses.DiceLoss(class_weights=unet.class_weights)
+# 	if loss == 'cross_entropy': unet.loss = sm.losses.categorical_crossentropy
 
-	unet.train(sample, val_sample, zac_samples)
-	unet.makeLossCurve()
-	del unet
+# 	unet.train(sample, val_sample, zac_samples)
+# 	unet.makeLossCurve()
+# 	del unet
 
 
 
