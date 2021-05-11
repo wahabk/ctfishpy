b237597de17b01e7b313a9ba473240358035d548
wahabk <akawafi3@gmail.com>
2021-05-11 10:22:41 +0100

    man loc

--- fix_cc.py
+++ fix_cc.py
@@ -10,20 +10,18 @@ import json
 if __name__ == "__main__":
 	ctreader = ctfishpy.CTreader()
 	datapath = ctreader.centres_path
+	fishnums = ctreader.fish_nums
 
 	# wahab_samples 	= [200,218,240,277,330,337,341,462,40,78, 464,385] # cc doesnt work on 464 385
 	# mariel_samples	= [242,256,259,459,463,530,589,421,423] # 421 423 removed
 	# zac_samples		= [257,443,461,527,582]
 
-	# sample = wahab_samples+mariel_samples+zac_samples
-
 	# with open(datapath, 'r') as fp:
 	# 	data = json.load(fp)
 	# done = list(data.keys())
 
-	sample=[530]
-
-	for fish in sample:
+	data={}
+	for fish in fishnums:
 		# if str(fish) not in done:
 			print(fish)
 			positions = ctreader.cc_fixer(fish)
