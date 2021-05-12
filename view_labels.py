b1ee56bb76359c863661b0abf329a2bc518b1117
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2021-05-12 12:00:25 +0100

    view

--- view_labels.py
+++ view_labels.py
@@ -9,11 +9,12 @@ from pathlib2 import Path
 
 mariele_sample 	= [40,242,256,259,421,423,459,463,530,589]
 zac_sample 		= [40,242,256,257,421,423,443,461,527,582]
-organ = 'Otoliths_Mariele'
+my_sample		= [78,200,218,240,330,337,341,364,385,462,464]
+organ = 'Otoliths'
 ctreader = ctfishpy.CTreader()
 
 
-for n in mariele_sample:
+for n in my_sample:
 	ct, stack_metadata = ctreader.read(n, align=True)
 	datapath = ctreader.dataset_path
 	label = ctreader.read_label(organ, n, align=False, is_amira=True)
