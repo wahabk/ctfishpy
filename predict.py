5e36f0deae8ab22fd2dff193eafa0df4e49de850
wahabk <akawafi3@gmail.com>
2021-05-24 13:49:25 +0100

    with open and memory leak tracker

--- predict.py
+++ predict.py
@@ -3,18 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-ctreader = ctfishpy.CTreader()
-unet = ctfishpy.Unet('Otoliths')
-unet.weightsname = 'new_roi'
-nums = ctreader.fish_nums
-# random.shuffle(nums)
+# checking for memory leaks
+from pympler.tracker import SummaryTracker
+tracker = SummaryTracker()
 
-for n in nums:
-	print(n)
-	label, ct = unet.predict(n, thresh = 0.3)
-	print(label.shape, np.max(label))
 
-	ctreader.write_label('Otoliths_unet2d', label, n)
-	# ctreader.make_gif(ct[1200:1500], 'output/test_labels.gif', fps=30, label = label[1200:1500])
-	# ctreader.view(ct,label)
+
+if __name__ == '__main__':
+	ctreader = ctfishpy.CTreader()
+	unet = ctfishpy.Unet('Otoliths')
+	unet.weightsname = 'new_roi'
+	nums = ctreader.fish_nums
+	# random.shuffle(nums)
+
+	for n in nums:
+		print(n)
+		label, ct = unet.predict(n, thresh = 0.3)
+		print(label.shape, np.max(label))
+
+		ctreader.write_label('Otoliths_unet2d', label, n)
+		tracker.print_diff()
+		# ctreader.make_gif(ct[1200:1500], 'output/test_labels.gif', fps=30, label = label[1200:1500])
+		# ctreader.view(ct,label)
 
