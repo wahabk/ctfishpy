fd9ab7c8ab351b20a8e75224761bc28a21cda44c
Abdelwahab Kawafi <a.kawafi@bristol.ac.uk>
2021-05-24 13:52:29 +0100

    collect garbage

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import ctfishpy
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import gc
 
 ctreader = ctfishpy.CTreader()
 unet = ctfishpy.Unet('Otoliths')
@@ -13,8 +14,9 @@ for n in nums:
 	print(n)
 	label, ct = unet.predict(n, thresh = 0.3)
 	print(label.shape, np.max(label))
+	gc.collect()
 
-	ctreader.write_label('Otoliths_unet2d', label, n)
+	# ctreader.write_label('Otoliths_unet2d', label, n)
 	# ctreader.make_gif(ct[1200:1500], 'output/test_labels.gif', fps=30, label = label[1200:1500])
 	# ctreader.view(ct,label)
 
