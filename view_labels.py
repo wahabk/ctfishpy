da30175ce2e8e4ed9ab90e13e0bbec45d8b2f2ac
wahabk <akawafi3@gmail.com>
2021-10-13 13:48:08 +0100

    fix view labels

--- view_labels.py
+++ view_labels.py
@@ -20,21 +20,21 @@ check = mariele_samples+zac_samples+my_samples
 
 segs = 'Otoliths_unet2d'
 
-for n in [421,582, 461, 464, 583, 584, 585, 586, 587]:#ctreader.fish_nums[ctreader.fish_nums.index(87)+1:]:
+for n in [40, 421,582, 461, 464, 583, 584, 585, 586, 587]:#ctreader.fish_nums[ctreader.fish_nums.index(87)+1:]:
 	
 	center = ctreader.manual_centers[str(n)]
 	roiSize = (192,288)
 
 	roiZ = 128
+	ct, stack_metadata = ctreader.read(n, align=True)
+	ctreader.view(ct) #, label=label)
 
-	label = ctreader.read_label(segs, n, align = False, is_amira=False)
-	label = ctreader.crop_around_center3d(label, roiSize, center, roiZ=roiZ)
+	# label = ctreader.read_label(segs, n, align = False, is_amira=False)
+	# label = ctreader.crop_around_center3d(label, roiSize, center, roiZ=roiZ)
 
-	z_center = center[0]
+	# z_center = center[0]
 	
-	ct, stack_metadata = ctreader.read(n, r = (z_center - int(roiZ/2), z_center + int(roiZ/2)), align=True)
-	center[0] = int(roiZ/2)
-	ct = ctreader.crop_around_center3d(ct, roiSize, center, roiZ=roiZ)
+	# center[0] = int(roiZ/2)
+	# ct = ctreader.crop_around_center3d(ct, roiSize, center, roiZ=roiZ)
 	
-	ctreader.view(ct, label=label)
-	# ctreader.make_gif(ct[1200:1500], 'output/test_labels.gif', fps=20, label = label[1200:1500])
+	# # ctreader.make_gif(ct[1200:1500], 'output/test_labels.gif', fps=20, label = label[1200:1500])
