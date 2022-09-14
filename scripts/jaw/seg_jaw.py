815f4f47994d9d775ac80775e469a8f74de121de
wahabk <akawafi3@gmail.com>
2022-09-14 11:00:32 +0100

    seg_jaw in 2d as well

--- scripts/jaw/seg_jaw.py
+++ scripts/jaw/seg_jaw.py
@@ -14,11 +14,11 @@ from skimage.segmentation import flood
 @magicgui(auto_call=True,
 	threshold={"widget_type": "Slider", "max":255, "min":0},
 	new_value={"widget_type": "SpinBox", "max":255, "min":0},
-	only_in={"widget_type": "CheckBox"},
+	TwoD={"widget_type": "CheckBox"},
 	seek={"widget_type": "CheckBox"},
 	# reset_center={"widget_type": "PushButton"},
 	layout='Horizontal',)
-def labeller(layer:Layer, label_layer:Labels, threshold:int=125, new_value:int=1, only_in:bool=False, seek=False) -> None: # reset_center:bool=False
+def labeller(layer:Layer, label_layer:Labels, threshold:int=125, new_value:int=1, TwoD:bool=False, seek=False) -> None: # reset_center:bool=False
 	if layer is not None:
 		if label_layer is not None:
 			assert isinstance(layer.data, np.ndarray)  # it will be!
@@ -30,16 +30,28 @@ def labeller(layer:Layer, label_layer:Labels, threshold:int=125, new_value:int=1
 			image = layer.data
 
 			point = layer.metadata['point']
+			_slice = layer.metadata['slice']
 			if point is not None:
 				if len(point) == 3:
-					point = tuple([int(x) for x in point])
-					new_label = None
-					new_label = flood(image, point, tolerance=threshold)
+					if TwoD == False:
+						point = tuple([int(x) for x in point])
+						new_label = None
+						new_label = flood(image, point, tolerance=threshold)
 
-					print(new_label.min(), new_label.max(), new_label.shape)
-					print(label.min(), label.max(), label.shape)
+						print(label.min(), label.max(), label.shape)
+						print(new_label.min(), new_label.max(), new_label.shape)
 
-					label_layer.data[new_label==True] = new_value
+						label_layer.data[new_label==True] = new_value
+					else:
+						point = tuple([int(x) for x in point[1:]])
+						label = label[_slice]
+						new_label = None
+						new_label = flood(image[_slice], point, tolerance=threshold)
+
+						print(label.min(), label.max(), label.shape)
+						print(new_label.min(), new_label.max(), new_label.shape)
+
+						label_layer.data[_slice, new_label==True] = new_value
 	
 	if seek == False:
 		layer.metadata['point'] = None
@@ -48,14 +60,18 @@ def labeller(layer:Layer, label_layer:Labels, threshold:int=125, new_value:int=1
 def create_labeller(viewer, layer) -> None:
 	widget = labeller
 	layer.metadata['point'] = None
+	
 
 	viewer.window.add_dock_widget(widget, name="labeller", area='right')
 	viewer.layers.events.changed.connect(widget.reset_choices)
+
+	#TODO current slice?
 
 	@layer.mouse_drag_callbacks.append
 	def get_event(layer, event):
 		if event.button == 2: # if left click
 			layer.metadata['point'] = event.position #flip because qt :(
+			layer.metadata['slice'] = int(layer.position[0]) # get the slice youre looking at
 			widget.update()
 		return
 
@@ -82,13 +98,16 @@ def label(scan):
 
 
 if __name__ == "__main__":
-	dataset_path = "/home/ak18001/Data/HDD/uCT"
+
+	# dataset_path = "/home/ak18001/Data/HDD/uCT"
+	dataset_path = "/home/wahab/Data/HDD/uCT"
+	
 	ctreader = ctfishpy.CTreader(dataset_path)
 
 	scan = ctreader.read(1)
 
 	scan = ctreader.to8bit(scan)
-	scan = scan[1000:]
+	# scan = scan[1000:]
 	# scan = zoom(scan, 0.5)
 
 	# point = (1368, 328, 356)
