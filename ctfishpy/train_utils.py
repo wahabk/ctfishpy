FIXED softmax was off!! augs done

ctfishpy/train_utils.py
@@ -46,7 +46,7 @@ class CTDataset(torch.utils.data.Dataset):
 	"""	
 
 	def __init__(self, dataset_path, bone:str, indices:list, roi_size:tuple, n_classes:int, 
-				transform=None, label_transform=None, label_size:tuple=None,
+				transform=None, label_size:tuple=None,
 				dataset:np.ndarray=None, labels:np.ndarray=None,  precached:bool=False):
 		super().__init__()
 		self.dataset_path = dataset_path
@@ -55,7 +55,6 @@ class CTDataset(torch.utils.data.Dataset):
 		self.roi_size = roi_size
 		self.n_classes = n_classes
 		self.transform = transform
-		self.label_transform = label_transform
 		self.label_size = label_size
 		self.precached = precached
 		if self.precached:
@@ -377,7 +376,7 @@ def test(dataset_path, model, bone, test_set, params, threshold=0.5, num_workers
 		label_size = roiSize
 
 	# test on real data
-	test_ds = CTDataset(dataset_path, bone, test_set, roiSize, n_classes, transform=None, label_transform=None, label_size=label_size) 
+	test_ds = CTDataset(dataset_path, bone, test_set, roiSize, n_classes, transform=None, label_size=label_size) 
 	test_loader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=torch.cuda.is_available())
 
 	losses = []
@@ -411,8 +410,12 @@ def test(dataset_path, model, bone, test_set, params, threshold=0.5, num_workers
 			# fig = plot_auc_roc(fpr, tpr, aucroc)
 			# run[f'AUC_{i}'].upload(fig)
 
-			aucroc = roc_auc_score(y_numpy.flatten(), y_pred_numpy.flatten(), average='weighted')
-			dice_score = monai.metrics.compute_generalized_dice(y_pred=y_pred, y=y, include_background=False)
+
+
+			aucroc = roc_auc_score(y_numpy.flatten(), y_pred_numpy.flatten(), average='weighted', multi_class='ovo')
+			dice_score = monai.metrics.compute_generalized_dice(y_pred=y_pred, y=y, include_background=False)[0].item()
+			threshed = y_pred[y_pred < threshold] = 0
+			threshed = y_pred[y_pred > threshold] = 1
 			iou = monai.metrics.compute_meaniou(y_pred=y_pred, y=y, include_background=False)
 
 			pred_label = undo_one_hot(y_pred_numpy, n_classes, threshold=threshold)
@@ -547,9 +550,9 @@ class Trainer:
 
 			if isinstance(self.criterion, torch.nn.BCEWithLogitsLoss) == False:
 				# print("testing final activation", self.n_classes, out.shape, target.shape)
-				# out = torch.softmax(out, 0)
+				out = torch.softmax(out, 1) # this breaks aging (classification) model!
 				# print(out, target)
-				pass
+
 			loss = self.criterion(out, target)  # calculate loss
 			loss_value = loss.item()
 			train_losses.append(loss_value)

scripts/training/b01.py
@@ -78,19 +78,17 @@ def train(dataset_path, config, name, bone, train_data, val_data, test_data, sav
 
 	run['Tags'] = name
 	
-	transforms_affine = tio.Compose([
+	transforms = tio.Compose([
 		tio.RandomFlip(axes=(0,1,2), flip_probability=0.25),
-		tio.RandomAffine(),
-	])
-	transforms_img = tio.Compose([
-		tio.RandomAnisotropy(p=0.2),              # make images look anisotropic 25% of times
+		tio.RandomAffine(p=0.25),
+		tio.RandomAnisotropy(p=0.3),              # make images look anisotropic 25% of times
 		tio.RandomBlur(p=0.3),
+		tio.RandomBiasField(0.4),
 		tio.OneOf({
 			tio.RandomNoise(0.1, 0.01): 0.1,
-			tio.RandomBiasField(0.1): 0.1,
 			tio.RandomGamma((-0.3,0.3)): 0.1,
-			tio.RandomMotion(): 0.3,
 		}),
+		tio.ZNormalization(),
 		tio.RescaleIntensity(percentiles=(0.5,99.5)),
 	])
 
@@ -100,18 +98,19 @@ def train(dataset_path, config, name, bone, train_data, val_data, test_data, sav
 	label_size = params['roiSize']
 
 	train_dataset, train_labels = precache(params['dataset_path'], params['train_data'], params['bone'], params['roiSize'])
+
 	print(train_dataset[0].shape, train_dataset[0].max())
 	# create a training data loader
 	# TODO remove precached
 	train_ds = CTDataset(dataset_path=params['dataset_path'], bone=params['bone'], indices=params['train_data'],
 						dataset=train_dataset, labels=train_labels, roi_size=params['roiSize'], n_classes=params['n_classes'], 
-						transform=transforms_img, label_transform=transforms_affine, label_size=label_size, precached=True) 
+						transform=transforms, label_size=label_size, precached=True) 
 	train_loader = torch.utils.data.DataLoader(train_ds, batch_size=params['batch_size'], shuffle=False, num_workers=params['num_workers'], pin_memory=torch.cuda.is_available(), persistent_workers=True)
 	# create a validation data loader
 	val_dataset, val_labels = precache(params['dataset_path'], params['val_data'], params['bone'], params['roiSize'])
 	val_ds = CTDataset(dataset_path=params['dataset_path'], bone=params['bone'], indices=params['val_data'],
 					dataset=val_dataset, labels=val_labels, roi_size=params['roiSize'], n_classes=params['n_classes'], 
-					transform=None, label_transform=None, label_size=label_size, precached=True) 
+					transform=None, label_size=label_size, precached=True) 
 	val_loader = torch.utils.data.DataLoader(val_ds, batch_size=params['batch_size'], shuffle=False, num_workers=params['num_workers'], pin_memory=torch.cuda.is_available(), persistent_workers=True)
 
 	# device
@@ -199,19 +198,20 @@ if __name__ == "__main__":
 	bone = 'OTOLITHS'
 
 	old_ns = [40, 78, 200, 218, 240, 242, 257, 259, 277, 330, 337, 341, 364, 385, 421, 423, 443, 459, 461, 462, 463, 464, 527, 530, 582, 589] 
-	all_keys = [1, 39, 64, 74, 96, 98, 112, 113, 115, 133, 186, 193, 197, 220, 241, 275, 276, 295, 311, 313, 314, 315, 316, 371, 374, 420, 427] # 371,374 ncoa3 420, 427 col11
-
+	all_keys = [1, 39, 64, 74, 96, 98, 112, 113, 115, 133, 186, 193, 197, 220, 241, 275, 276, 295, 311, 313, 314, 315, 316, 371, 374, 420, 427] 
+	crazy_fish = [371, 374, 420, 427] # 371,374 ncoa3 420, 427 col11
+	[all_keys.remove(i) for i in crazy_fish]
 
 	print(f"All data: {len(all_keys)}")
 
 	random.seed(42)
 	random.shuffle(all_keys)
-	test_data = [311, 316, 420] # val on young, mid and old col11
+	test_data = [1, 311, 316] # val on young, mid and old col11
 	[all_keys.remove(i) for i in test_data]
-	# train_data = all_keys[0:20]
-	# val_data = all_keys[20:24]
-	train_data = all_keys[1:2]
-	val_data = all_keys[2:3]
+	train_data = all_keys[:18]
+	val_data = all_keys[18:]
+	# train_data = all_keys[1:2]
+	# val_data = all_keys[2:3]
 	print(f"train = {train_data} val = {val_data} test = {test_data}")
 	name = 'fin train?'
 	save = False
@@ -222,20 +222,19 @@ if __name__ == "__main__":
 	#TODO use seg models pytorch
 
 	config = {
-		"lr": 4e-3,
+		"lr": 3e-3,
 		"batch_size": 4,
-		"n_blocks": 2,
+		"n_blocks": 3,
 		"norm": 'INSTANCE',
-		"epochs": 1,
+		"epochs": 50,
 		"start_filters": 32,
-		"activation": "RELU",
-		"dropout": 0.1,
-		"loss_function": monai.losses.TverskyLoss(include_background=True, alpha=0.3), #k monai.losses.DiceLoss(include_background=False,) #monai.losses.TverskyLoss(include_background=True, alpha=0.7) # # #torch.nn.CrossEntropyLoss()  #  torch.nn.BCEWithLogitsLoss() #BinaryFocalLoss(alpha=1.5, gamma=0.5),
+		"activation": "PRELU",
+		"dropout": 0,
+		"loss_function": monai.losses.TverskyLoss(include_background=True, alpha=0.7), #k monai.losses.DiceLoss(include_background=False,) #monai.losses.TverskyLoss(include_background=True, alpha=0.7) # # #torch.nn.CrossEntropyLoss()  #  torch.nn.BCEWithLogitsLoss() #BinaryFocalLoss(alpha=1.5, gamma=0.5),
 	}
 
 	# TODO add model in train
 
 	work_dir = Path().parent.resolve()
-
 	train(dataset_path, config, name, bone=bone, train_data=train_data, val_data=val_data, 
 			test_data=test_data, save=save, tuner=False, device_ids=[0,], num_workers=10, work_dir=work_dir)

tests/test_datagenie.py
@@ -4,6 +4,7 @@ from ctfishpy.train_utils import CTDataset, undo_one_hot, renormalise
 import torchio as tio
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 
@@ -18,22 +19,22 @@ if __name__ == "__main__":
 	n_classes = 4
 	num_workers = 10
 	all_keys = [1, 39, 64, 74, 96, 98, 112, 113, 115, 133, 186, 193, 197, 220, 241, 275, 276, 295, 311, 313, 314, 315, 316, 371, 374, 420, 427]
-	check_set = all_keys[:]
-
+	random.shuffle(all_keys)
+	check_set = all_keys[:1]
 	print(f"checking dataset {check_set}")
 
 	transforms = tio.Compose([
-		tio.RandomFlip(axes=(0,1,2), flip_probability=0.8),
-		tio.RandomAffine(),
+		tio.RandomFlip(axes=(0,1,2), flip_probability=0.5),
+		tio.RandomAffine(p=0.5),
 		tio.RandomAnisotropy(p=0.2),              # make images look anisotropic 25% of times
 		tio.RandomBlur(p=0.2),
+		tio.RandomBiasField(0.4),
 		tio.OneOf({
-			tio.RandomNoise(0.1, 0.01): 0.1,
-			tio.RandomBiasField(0.1): 0.1,
-			tio.RandomGamma((-0.3,0.3)): 0.1,
-			tio.RandomMotion(): 0.3,
+			tio.RandomNoise(0.1, 0.01): 0.2,
+			tio.RandomGamma((-0.3,0.3)): 0.2,
+			tio.ZNormalization(): 0.2,
 		}),
-		tio.RescaleIntensity(percentiles=(0.5,99.5)),
+		tio.RescaleIntensity(),
 	])
 
 	check_ds = CTDataset(dataset_path, bone, check_set, roiSize, n_classes=n_classes, transform=transforms, label_size=roiSize) 
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 	
 	while True:
 		x, y = next(iter(check_loader))
-
+		
 		# print(example[0].shape)
 		# print(example[1].shape)
 
@@ -61,6 +62,20 @@ if __name__ == "__main__":
 		print(type(x))
 
 		ctreader.view(x, label=y)
+
+		x = x/x.max()
+
+		histogram, bin_edges = np.histogram(x, bins=256, range=(0, 1))
+
+		plt.figure()
+		plt.title("Grayscale Histogram")
+		plt.xlabel("grayscale value")
+		plt.ylabel("pixel count")
+		plt.xlim([0.0, 1.0])  # <- named arguments do not work here
+
+		plt.plot(bin_edges[0:-1], histogram)  # <- or here
+
+		plt.show()
 
 		# array_projection = np.max(x, axis=0)
 		# label_projection = np.max(y, axis=0)