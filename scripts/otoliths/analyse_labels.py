14f69e8e95cb6ebcdf2241516f61789bcedab26d
wahabk <akawafi3@gmail.com>
2022-10-24 15:04:43 +0100

    analyse labels time

--- scripts/otoliths/analyse_labels.py
+++ scripts/otoliths/analyse_labels.py
@@ -20,9 +20,49 @@ if __name__ == '__main__':
 
 	ctreader = ctfishpy.CTreader(dataset_path)
 	master = ctreader.master
+	missing = list(master.loc[:320].index)
 
-	broken = np.array([276, 277, 278, 279, 280, 318, 319, 320])
+	data_path = "output/results/3d_unet_data20221020.csv"
+	new_data_path = "output/results/3d_unet_data20221024.csv"
+	df = pd.read_csv(data_path, index_col=0)
+	print(df)
+
+	print(missing)
+
+	data_dict = {}
+	n_classes = 4
+	for n in missing[:2]:
+		print(n)
+		metadata = ctreader.read_metadata(n)
+		scan = ctreader.read(n)
+		label = ctreader.read_label("Otolith_unet", n)
+
+		dens = ctreader.getDens(scan, label, n_classes)
+		vols = ctreader.getVol(label, metadata, n_classes)
+
+		print(dens, vols)
+
+		data_dict[n] = {
+			"Dens1" : dens[0],
+			"Dens2" : dens[1],
+			"Dens3" : dens[2],
+			"Vol1" : vols[0],
+			"Vol2" : vols[1],
+			"Vol3" : vols[2],
+		}
+
+	new_df = pd.DataFrame.from_dict(data_dict, orient='index')
+	print(new_df)
+
+	final = pd.concat([new_df, df])
+
+	print(final)
+	# final.to_csv(new_data_path)
 
 
 
-    
+
+
+
+
+
